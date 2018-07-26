7259b96d4c2899f979c0aa01d9a035dbdb543a8d
Takuya Kishira <takuya.kishira@livesense.co.jp>
2018-07-26 19:00:11 +0900

    [add]タグ一覧ページの追加

--- src/assets/js/helper.js
+++ src/assets/js/helper.js
@@ -10,6 +10,22 @@ modules.helper = (function () {
    * abc defg hij -> [a]bc [d]efg [h]ij
    * のように各要素の先頭文字をタグつけする
    */
+  module.getStrage = function(){
+    if(('localStorage' in window) && (window.localStorage !== null)) {
+      return localStorage;
+    } else {
+      toastr.error('ローカルストレージが使用できません。');
+      return {
+        getItem: function(key, val){},
+        setItem: function(key){},
+      }
+    }
+  }
+
+  /**
+   * abc defg hij -> [a]bc [d]efg [h]ij
+   * のように各要素の先頭文字をタグつけする
+   */
   module.eachTopCharUpper = function(str, startTag, endTag){
     var strAry = str.split(' ');
     strAry.forEach(function(val,i,ar){
@@ -18,6 +34,28 @@ modules.helper = (function () {
     });
 
     return strAry.join(' ');
+  }
+
+
+  /**
+   * ＃or空白区切りの文字列を「#1#2#3」の形式で返却する
+   */
+  module.formatTag = function(str){
+    if(!str || str.length == 0){ return ""; }
+
+    // 前後の空白を削除
+    str = str.replace(/^\s+/, '');
+    str = str.replace(/\s*$/, '');
+
+    // 先頭に必ず＃をつける
+    str = "#" + str.replace(/^#/, '');
+
+    str = str.replace(/#/g, ' ');
+    str = str.replace(/\s+/g, '#');
+
+    // 余っている＃を削除
+    str = str.replace(/#$/g, '');
+    return str;
   }
 
   return module;
