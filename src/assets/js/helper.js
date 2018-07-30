c632e7741470ab12017eee5228c2610805909336
Ryuji <taku.kisi.1989@gmail.com>
2018-07-31 00:38:49 +0900

    [update]微修正

--- src/assets/js/helper.js
+++ src/assets/js/helper.js
@@ -59,5 +59,14 @@ modules.helper = (function () {
     return str;
   }
 
+  /**
+   * 空白、タグ、改行を除去した文字列を返却
+   */
+  module.excludeBlank = function(str){
+    if(!str || str.length == 0){ return ""; }
+
+    return str.replace(/\s|\n|\t/g, '');
+  }
+
   return module;
 }());
